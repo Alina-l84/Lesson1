b5f61015079858671ce2896109758f6bafaf583b
Alina-l84 <Alina-l84@mail.ru>
2023-04-30 18:29:31 +0300

    creat file les1

--- Les1.py
+++ Les1.py
@@ -1,1 +1,15 @@
-pffghdjkljgld
+a = input("ВВедите число 1 ")
+b = input("ВВедите число  2 ")
+try:
+    print("Результат:", int(a)/int(b))
+except ValueError:
+    print("Вводите только числа")
+except TypeError:
+    print("На ноль делить нельзя")
+finally:
+    print("Конструкция сработала!")
+
+
+
+
+
